0ee5d68da122051ac1e32a26c6cf97fd6294c8cd
sarabpj <sjay02@yahoo.com>
2016-08-24 14:19:50 -0700

    updated migrations, view is working

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='../client/views', static_folder="../client")
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/journal'
@@ -30,28 +31,40 @@ class User(db.Model):
 
 
 
-class Journal(db.Model):
+class Entry(db.Model):
 
   __tablename__ = 'entries'
 
   id = db.Column(db.Integer, primary_key=True)
   text = db.Column(db.Text(), nullable=False)
-  time_created = db.Column(db.TIMESTAMP(timezone=False))
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  time_created = db.Column(db.TIMESTAMP(timezone=False))
+  result_text_sentiment = db.Column(JSON)
+  result_stop_words = db.Column(JSON)
 
-  def __init__(self,text, user_id, time_created):
+  def __init__(self,text, user_id, time_created, result_text_sentiment,result_stop_words):
     self.text = text
     self.user_id = user_id
-    self.time_created
+    self.time_created = time_created
+    self.result_text_sentiment = result_text_sentiment
+    self.result_stop_words = result_stop_words
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 
+@app.route('/entry')
+def new_entry():
+    return render_template('index.html')
+
+
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
