4e42af3ef99f194f54ea736b3030f3c4ba71c957
sarabpj <sjay02@yahoo.com>
2016-08-29 10:05:52 -0700

    new migration

--- server/app.py
+++ server/app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON
 import os
+import nltk
 
+nltk.download()
 
 app = Flask(__name__,template_folder='../client/views', static_folder="../client")
 
@@ -42,13 +44,24 @@ class Entry(db.Model):
   result_text_sentiment = db.Column(JSON)
   result_stop_words = db.Column(JSON)
 
-  def __init__(self,text, user_id, time_created, result_text_sentiment,result_stop_words):
+  def __init__(self,text, time_created, result_text_sentiment,result_stop_words,user_id=None,):
     self.text = text
-    self.user_id = user_id
     self.time_created = time_created
     self.result_text_sentiment = result_text_sentiment
     self.result_stop_words = result_stop_words
+    self.user_id = user_id
 
+
+example_text = "Hello there my name is sara how are you doing today. it is a interesting time to be alive in our world. i live in a country with a ton of opportunity if you have the right skillset sadly i am still struggling to find a career"
+
+def get_stop_words(text):
+  # tokenize words
+  pass
+
+def get_text_sentiment(text):
+  pass
+
+# routes
 @app.route('/')
 def hello():
     return "Hello World!"
@@ -59,10 +72,9 @@ def new_entry():
     return render_template('index.html')
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
+@app.route('/<path:path>')
+def catch_all(path):
+    pass
 
 
 
